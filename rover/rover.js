3a7c7e7687f8b6a6857fa740378d333fa2f24efb
mala23 <mail@mala23.me>
2014-11-30 19:41:23 +0000

    rover has a board

--- rover/rover.js
+++ rover/rover.js
@@ -1,7 +1,9 @@
 var five = require('johnny-five');
 
 function Rover() {
+
   this.board = new five.Board();
+
 };
 
 module.exports = Rover;
--- test/rover/roverTest.js
+++ test/rover/roverTest.js
@@ -1,12 +1,13 @@
 var expect = require('chai').expect;
-var rover = require('../../rover/rover.js');
+var Rover = require('../../rover/rover.js');
 
 describe('Rover', function() {
 
   describe('Setup', function() {
 
     it('Has a board', function() {
-      expect(rover.board).to.equal(true);
+      var rover = new Rover;
+      expect(rover.board).to.be.truthy;
     });
 
   });
