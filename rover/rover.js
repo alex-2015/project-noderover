5d9f141528ae8ab2b311b4afc288d8d36d10fb37
mala23 <mail@mala23.me>
2014-12-01 11:43:49 +0000

    broken

--- rover/rover.js
+++ rover/rover.js
@@ -1,9 +1,19 @@
 var five = require('johnny-five');
 
 function Rover() {
-
   this.board = new five.Board();
+}
+
+  this.board.on('ready', function() {
+
+    .motor01 = new five.Motor(); 
+
+  });
 
 };
 
+var rover = new Rover;
+
+console.log(rover);
+console.log(rover.board);
 module.exports = Rover;
--- test/rover/roverTest.js
+++ test/rover/roverTest.js
@@ -5,9 +5,15 @@ describe('Rover', function() {
 
   describe('Setup', function() {
 
-    it('Has a board', function() {
+    it('has a board', function() {
       var rover = new Rover;
+      console.log(rover);
       expect(rover.board).to.be.truthy;
+    });
+
+    it('has a motor01', function() {
+      var rover = new Rover;
+      expect(rover.motor01).not.to.be.undefined;
     });
 
   });
